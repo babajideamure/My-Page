c43a1e00eadd39b2628e5e41d64f7e2ae6406da3
Babajide Amure <145385180+babajideamure@users.noreply.github.com>
2025-01-31 14:55:36 +0100

    Fist Page Upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let menu = document.querySelector("#header ul");
+let bar = document.querySelector("#header .fa-bars");
+
+bar.addEventListener("click", function() {menu.classList.toggle("show");});
